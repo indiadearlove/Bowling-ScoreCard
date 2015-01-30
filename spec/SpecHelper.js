d0dffcb4fc7c261dc74fee48c2a126a802492bcc
India Dearlove <india_dearlove@yahoo.co.uk>
2015-01-30 11:43:14 +0000

    Created table

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,1 +1,19 @@
-SpecHelper.js
+describe('BowlingScoreCard', function(){
+
+  var scorecard;
+  beforeEach(function(){
+    scorecard = new ScoreCard();
+  });
+
+  describe("new game", function(){
+
+    it("starts with score 0", function(){
+      expect(scorecard.score()).toEqual(0);
+    });
+
+    it("can have a player name", function(){
+
+    });
+  })
+
+});
